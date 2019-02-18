c8e65be810891fde867472c9cb77fd25aec6870d
vastronoto <38988126+vastronoto@users.noreply.github.com>
2019-02-17 21:49:28 -0600

    Add files via upload

---
+++ group_16_assignment3/README.txt
@@ -0,0 +1,13 @@
+// README //
+
+1. Make sure that a .txt file of the novel of interest (in our case, The Adventures of Tom Sawyer labeled Tom.txt) is in the same directory as extract_words.py.
+
+2. Open and run extract_words.py. Three text files will result in the directory: allwords.txt containing all words in the text, uniquewords.txt containing all unique words in the text, and wordfrequency containing key-value pairs of the number of words that appear and the specific number of times.
+
+3. Open the a3_novelvisualization directory, and make sure that the desired .ttf or .otf font file (Old Press.ttf), desired background image (Paper.jpg), and uniquewords.txt are there along with a3_novelvisualization.pde.
+
+4. Run a3_novelvisualization.pde and generate the word cloud. 
+
+5. Open the a3_wordfrequency directory, and make sure that wordfrequency.txt is there along with a3_wordfrequency.pde.
+
+6. Run a3_wordfrequency.pde and generate a visualization of the word frequency. 
---
+++ group_16_assignment3/extract_words.py
@@ -0,0 +1,69 @@
+import re
+word_dict={}
+
+def uniquewords(allwords):
+    out_file2=open('./uniquewords.txt',"w",encoding="utf8")
+    uniquelist=[]
+    for word in allwords:
+        if word not in uniquelist:
+            uniquelist.append(word)
+            out_file2.write(word+'\n')
+    out_file2.close()
+
+def wordfrequency(allwords):
+    out_file3=open('./wordfrequency.txt',"w",encoding="utf8")
+    word_freq={}
+    for word in allwords:
+        if word in word_dict.keys():
+            word_dict[word]+=1
+        else:
+            word_dict[word]=1
+    for word in word_dict:
+        frequency=word_dict[word]
+        if frequency in word_freq.keys():
+            word_freq[frequency]+=1
+        else:
+            word_freq[frequency]=1
+    for freq in word_freq.keys():
+        out_file3.write(str(freq)+': '+str(word_freq[freq])+'\n')
+    
+            
+    out_file3.close()
+            
+    
+            
+            
+            
+    
+    
+    
+
+
+def main():
+
+    in_file=open("./Tom.txt","r",encoding="utf8")
+    out_file1=open('./allwords.txt',"w",encoding="utf8")
+    allwords=[]
+    for line in in_file:
+        line=line.split()
+        for wordlist in line:
+            wordlist=wordlist.lower()
+            wordlist=re.findall('[a-z]+',wordlist)
+            for word in wordlist:
+                allwords.append(word)
+                
+                out_file1.write(word+'\n')
+    uniquewords(allwords)
+    wordfrequency(allwords)
+    in_file.close()
+    out_file1.close()
+
+
+
+
+
+main()
+        
+        
+    
+    
